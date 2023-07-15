0283c2e81babda5bf1932a470f9ab1b5320143f1
Raymond <raymond.w.lang@gmail.com>
2023-07-15 10:38:55 -0700

    set up User Schema

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import multer from "multer";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controller/register.js";
 
 // CONFIGURATIONS
 const __filname = fileURLToPath(import.meta.url);
@@ -32,6 +33,9 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
