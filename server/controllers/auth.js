3452028f397c26ae623c61fb1124692f70a7bb0b
Raymond <raymond.w.lang@gmail.com>
2023-07-15 10:54:38 -0700

    worked on login function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,9 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+// import dotenv from "dotenv";
+
+// dotenv.config();
 
 // REGISTER
 export const register = async (req, res) => {
@@ -33,6 +36,18 @@ export const register = async (req, res) => {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (err) {
-    res.status(500).json({error: err.message}))
+    res.status(500).json({ error: err.message });
   }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist." });
+    const isMatch = await bcrypt.compare(passwor, user.password);
+
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  } catch (err) {}
 };
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controller/register.js";
+import authRoutes from "./routes/auth.js";
 
 // CONFIGURATIONS
 const __filname = fileURLToPath(import.meta.url);
@@ -36,6 +37,9 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES
+app.use("/auth", authRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
