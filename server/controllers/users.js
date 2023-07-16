2b53db32ff4bcce0b9f92c57e90a2ca4918146fe
Raymond <raymond.w.lang@gmail.com>
2023-07-16 15:05:39 -0700

    added advertising widget

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,6 @@
 import User from "../models/User.js";
 
-// READ
+/* READ */
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
@@ -19,26 +19,18 @@ export const getUserFriends = async (req, res) => {
     const friends = await Promise.all(
       user.friends.map((id) => User.findById(id))
     );
-
     const formattedFriends = friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-        return {
-          _id,
-          firstName,
-          lastName,
-          occupation,
-          location,
-          picturePath,
-        };
+        return { _id, firstName, lastName, occupation, location, picturePath };
       }
     );
     res.status(200).json(formattedFriends);
-  } catch {
+  } catch (err) {
     res.status(404).json({ message: err.message });
   }
 };
 
-// UPDATE
+/* UPDATE */
 export const addRemoveFriend = async (req, res) => {
   try {
     const { id, friendId } = req.params;
@@ -47,31 +39,25 @@ export const addRemoveFriend = async (req, res) => {
 
     if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId);
-      friend.friends = user.friends.filter((id) => id !== id);
+      friend.friends = friend.friends.filter((id) => id !== id);
     } else {
       user.friends.push(friendId);
       friend.friends.push(id);
     }
     await user.save();
     await friend.save();
+
     const friends = await Promise.all(
       user.friends.map((id) => User.findById(id))
     );
-
     const formattedFriends = friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-        return {
-          _id,
-          firstName,
-          lastName,
-          occupation,
-          location,
-          picturePath,
-        };
+        return { _id, firstName, lastName, occupation, location, picturePath };
       }
     );
+
     res.status(200).json(formattedFriends);
-  } catch {
+  } catch (err) {
     res.status(404).json({ message: err.message });
   }
 };
