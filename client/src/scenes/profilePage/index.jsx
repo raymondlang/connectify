42cbe6eef9bcf12f0700af9b87edc4e0bd540bf7
Raymond <raymond.w.lang@gmail.com>
2023-07-17 12:22:56 -0700

    fixed profile page

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -8,7 +8,7 @@ import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 
-const ProfilePage = () => {
+const Profile = () => {
   const [user, setUser] = useState(null);
   const { userId } = useParams();
   const token = useSelector((state) => state.token);
@@ -57,4 +57,4 @@ const ProfilePage = () => {
   );
 };
 
-export default ProfilePage;
+export default Profile;
